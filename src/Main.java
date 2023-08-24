5d1f1ed8db8f28f2fd1fd7cba37da1bc1af8d510
Agzzel <sterneraxel@protonmail.com>
2023-08-24 14:32:18 +0200

    Added the remaining methods + a few tests

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
         Shelter s = new Shelter();
-        s.animalQueue = new LinkedList<>();
+
+        Dog d = new Dog("a");
+        Dog d1 = new Dog("b");
+        Cat c = new Cat("c");
+        Cat c1 = new Cat("d");
+
+        Animal[] ar = {d,d1,c,c1};
+        for (Animal animal : ar) {
+            s.enqueue(animal);
+        }
+        s.dequeueDog();
+        s.dequeueAny();
+        for(int i = 0; i < s.animalQueue.size(); i++){
+            System.out.println(s.animalQueue.get(i));
+        }
+
     }
 }
--- src/Shelter.java
+++ src/Shelter.java
@@ -2,13 +2,32 @@ import java.util.LinkedList;
 
 public class Shelter {
     Shelter(){}
-    public LinkedList<Animal> animalQueue;
+    public LinkedList<Animal> animalQueue = new LinkedList<>();
 
-    void enqueue(Animal animal){
+    public void enqueue(Animal animal){
         animalQueue.add(animal);
     }
 
-    void dequeueAny(){
+    public void dequeueAny(){
         animalQueue.poll(); //removes first element in the queue
     }
+
+    public void dequeueDog(){
+        for(Animal a : animalQueue){
+            if(a instanceof Dog){
+                animalQueue.remove(a);
+                break;
+            }
+        }
+    }
+
+    public void dequeueCat(){
+        for(Animal a : animalQueue){
+            if(a instanceof Cat){
+                animalQueue.remove(a);
+                break;
+            }
+        }
+    }
+
 }
